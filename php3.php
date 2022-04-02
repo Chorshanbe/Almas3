74ca45f3ed3b8fa269a12d54c7eb2b098b256980
Chorshanbe <102850672+Chorshanbe@users.noreply.github.com>
2022-04-02 15:25:05 +0600

    Add files via upload

---
+++ php3.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+</head>
+<body>
+	<?php
+	$str =substr("qwerty",0,-3);
+	echo "$str";
+	?>
+
+</body>
+</html>
